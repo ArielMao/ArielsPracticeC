2361c7510bd4c4470f0fb6f7c72fccc8cc8fbde7
ArielMao <mjjkyy@qq.com>
2018-07-18 00:28:32 +0800

    20180717第一天练习，helloworld，调试并运行成功。 教程看到C数据类型 http://www.runoob.com/cprogramming/c-data-types.html

---
+++ test/src/hello.cpp
@@ -0,0 +1,26 @@
+//============================================================================
+// Name        : test.cpp
+// Author      : ArielM
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+	//char[] str = "helloworld";
+	//cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+    printf("helloworld\n");
+	return 0;
+}
+/*
+1. 保存文件为 hello.c。
+2. 打开命令提示符，进入到保存文件所在的目录。
+3. 键入 gcc hello.c，输入回车，编译代码。
+4. 如果代码中没有错误，命令提示符会跳到下一行，并生成 a.out 可执行文件。
+5. 现在，键入 a.out 来执行程序。
+6. 您可以看到屏幕上显示 "Hello World"。
+*/
